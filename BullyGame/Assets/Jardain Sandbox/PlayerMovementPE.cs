7ca52b49d64a0b9af186d2ed06e92de58f34f068
dapurpkilla <jlk5717@louisiana.edu>
2017-03-07 16:09:22 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	BullyGame/Library/CurrentLayout.dwlt
    #	BullyGame/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	BullyGame/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	BullyGame/Library/assetDatabase3
    #	BullyGame/Library/shadercompiler-UnityShaderCompiler.exe0.log

--- BullyGame/Assets/Jardain Sandbox/PlayerMovementPE.cs
+++ BullyGame/Assets/Jardain Sandbox/PlayerMovementPE.cs
@@ -19,14 +19,14 @@ public class PlayerMovementPE : MonoBehaviour {
         grounded,
         jumping,
         running,
-        crouching,
+        crouching
     }
-    public enum FightingState{
+    public enum FightingState
+    {
         attack,
         block,
         moving
     }
-
 	// Use this for initialization
 	void Start () {
         // Get Character Controller if it exists, if not create one in the object.
@@ -44,8 +44,9 @@ public class PlayerMovementPE : MonoBehaviour {
         movement = transform.TransformDirection(movement);
         movement.x *= horizontalMoveSpeed;
         movement.z = 0 ;
-        if (Input.GetButtonDown("Jump")) movement.y = jumpStrength;
-        else movement.y -= gravity * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.UpArrow)) verticalMoveSpeed = jumpStrength;
+        else verticalMoveSpeed -= gravity * Time.deltaTime;
+        movement.y = verticalMoveSpeed;
         player.Move(movement * Time.deltaTime);
 	}
     
